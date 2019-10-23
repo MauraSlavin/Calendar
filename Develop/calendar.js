37a1236ca661f04d6e1445ee7a39b7b83914e0a8
Maura Slavin <MauraMSlavin@gmail.com>
2019-10-23 17:51:07 -0400

    Display agenda items from local storage (or defaults for testing) for each time slot in editable textarea.

--- Develop/calendar.js
+++ Develop/calendar.js
@@ -1,53 +1,48 @@
-var date = moment().format('LL');
-$("#currentDay").text(date);
-var agendaJSON;
-var agenda = {
-    9: "Arrive at office",
-    10: "Check email",
-    11: "Answer letters",
-    12: "lunch",
-    13: "Do some coding",
-    14: "Break",
-    15: "Code review",
-    16: "Clean up",
-    17: "Go home"
-};
+var date = moment().format('LL');   // get today
+var agenda;        // agenda is an object.  keys are times of day, values are agenda items
+var agendaJSON;         // Object "agenda" as a JSON
 var agendaItems;  // array of agenda items [[9, "arrive"], [10,"email"]...]
 var time;         //  what time slot working on
 var agendaItem;           // agenda item (text) for this time slot
 
-// var object = JSON.parse(localStorage.getItem("object-json"));
+$("#currentDay").text(date);        // put today's date on the webpage
 
-agendaJSON = JSON.stringify(agenda);
-localStorage.setItem("agenda", agendaJSON);
-console.log("agenda:  " + agenda);
-console.log("agendaJSON:  " + agendaJSON);
-// alert("Check localStorage");
+// get agenda from local Storage
+agendaJSON = localStorage.getItem("agenda");
+agenda = JSON.parse(agendaJSON);
 
+if (agenda == null) {
+    agenda = {                      // object agenda with each hour slot and text 
+        9: "Arrive at office",           // values assigned for testing purposes
+        10: "",
+        11: "Answer letters",
+        12: "lunch",
+        13: "Do some coding",
+        14: "Break",
+        15: "Code review",
+        16: "Clean up",
+        17: "Go home"
+    };
+}
+
+// pull agenda items from the object
 agendaItems = Object.entries(agenda);
-console.log("agendaItems:  " + agendaItems);
 
-console.log("begin to loop through agenda items")
+// for each agenda item, set the placeholder in the textarea to the agenda item that was saved in local storage
 const keys = Object.entries(agendaItems);
 for (const key of keys) {                    // get array of [time, agendaitem] for each time slot
     time = key[1][0];           // the key is the time slot ("9" for 9am, etc)
-    agendaItem = key[1][1];
-    $("#time" + time).text(agendaItem);
-
+    agendaItem = key[1][1];     // the value is the agenda item text
+    $("#agenda" + time).attr("placeholder", agendaItem);   // change the text in the textarea (the placeholder)
 };
-    
 
 
 
-//agenda.time9 = "Forget emails";
-//$("#time9").text(agenda.time9);
-//console.log()
 
 
+// clear what was in localStorage, and store new information
+localStorage.clear();
 
-agendaJSON = localStorage.getItem("agenda");
-agenda = JSON.parse(agendaJSON);
-console.log("agendaJSON from localStorage:  " + agendaJSON);
-console.log("agenda from localStorage:  " + agenda);
-localStorage.removeItem("agenda");
-// alert("check localStorage - agenda should be gone");
+// Store agenda in localStorage
+agendaJSON = JSON.stringify(agenda);
+localStorage.setItem("agenda", agendaJSON);
